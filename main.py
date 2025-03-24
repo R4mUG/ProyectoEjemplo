03030243c4bd989540328779af2975e1fa299925
user-dam <dam65@dam65>
2025-03-24 16:16:12 +0100

    arreglos

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ def main(page: ft.Page):
     page.title = "Mi primera aplicación con Flet"
     page.add(ft.Text("¡Hola, caracola!"))
 
-ft.app(target=main)
-
 if __name__ == '__main__':
     ft.app(target=main, view=ft.WEB_BROWSER, port=8080)
 
