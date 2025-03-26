f097bc58cb6feb3b9f1c6dbf9d6fbf0d77a0dfe9
user-dam <dam65@dam65>
2025-03-26 19:07:20 +0100

    puerto

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ def main(page: ft.Page):
     page.add(ft.Text("¡Hola, caracola!"))
 
 if __name__ == '__main__':
-    ft.app(target=main, view=ft.WEB_BROWSER, port=8080)
+    ft.app(target=main, view=ft.WEB_BROWSER, port=30041)
 
